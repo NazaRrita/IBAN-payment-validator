1f4663170749dc17fc98604c0f64a1d07fb52250
Rita Nazarova <Rbatvinova@gmail.com>
2024-06-04 12:27:16 +0300

    Initial commit

---
+++ src/main/java/com/sample/ibanpaymentvalidator/IbanPaymentValidatorApplication.java
@@ -0,0 +1,13 @@
+package com.sample.ibanpaymentvalidator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbanPaymentValidatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IbanPaymentValidatorApplication.class, args);
+    }
+
+}
